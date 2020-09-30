db39190376b62a83f79b030dbbf67b23f73ca338
loki212 <lokeshprasadjagatha@gmail.com>
2020-09-30 05:57:45 +0100

    Added Pipeline

--- src/main/java/org/dell/kube/pages/HomeController.java
+++ src/main/java/org/dell/kube/pages/HomeController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+    private String pageContent;
 
-    @GetMapping
-    public String getPage(){
-        return "Hello from page : YellowPages";
+    public HomeController(@Value("${page.content}") String pageContent){
+        this.pageContent=pageContent;
     }
 
+    @GetMapping
+    public String getPage() {
+        return "Hello from page : " + pageContent + " ";
+    }
 
 }
