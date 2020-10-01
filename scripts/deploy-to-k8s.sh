3f9f22d3c7f6dc1caff98607ff570882c86bee38
loki212 <lokeshprasadjagatha@gmail.com>
2020-10-01 08:37:29 +0100

    Scaling Added

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -ex
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
 
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
@@ -11,3 +13,4 @@ kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/flyway-configmap.yaml
 kubectl apply -f deployment/flyway-job.yaml
+
